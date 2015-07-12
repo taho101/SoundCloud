b1f3000e726a6e852a8f5b56eaac72e281785606
taho101 <tahirski@gmail.com>
2015-07-12 21:19:45 +0200

    Project Initialized

---
+++ SoundCloud/Client/SoundCloudClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+
+namespace SoundCloud.Client
+{
+    class SoundCloudClient
+    {
+
+        private RestClient client;
+
+        public SoundCloudClient()
+        {
+            client = new RestClient(Properties.Settings.Default.APIUrl);
+        }
+    }
+}
