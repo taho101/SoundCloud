28ed493dfa230571bb11c21afb8245d90cfc83ab
AlexanderYW <alexanderw0310@gmail.com>
2015-07-14 22:40:20 +0200

    Added TrackSlider, Loading indicator and made some global styling

---
+++ SoundCloud/Elements/loading_indicator.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoundCloud.Elements
+{
+    /// <summary>
+    /// Interaction logic for loading_indicator.xaml
+    /// </summary>
+    public partial class loading_indicator : UserControl
+    {
+        public loading_indicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
