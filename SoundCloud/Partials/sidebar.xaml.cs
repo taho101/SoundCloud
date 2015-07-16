7a667ac2c58d5c6f5cff2453204662b0528b3884
AlexanderYW <alexanderw0310@gmail.com>
2015-07-16 22:45:29 +0200

    Added more control buttons and started building sidebar and navigation system

---
+++ SoundCloud/Partials/sidebar.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoundCloud.Partials
+{
+    /// <summary>
+    /// Interaction logic for sidebar.xaml
+    /// </summary>
+    public partial class sidebar : Page
+    {
+        public sidebar()
+        {
+            InitializeComponent();
+        }
+    }
+}
