2c7abbc4261ca5ac0adf1ceebed4e05770637283
taho101 <tahirski@gmail.com>
2015-07-18 22:25:29 +0200

    Updated the needed information on the tracks
    If any other information is needed we can fetch it to the class

--- SoundCloud/Objects/Track.cs
+++ SoundCloud/Objects/Track.cs
@@ -10,8 +10,36 @@ namespace SoundCloud.Objects
     {
         public int id {get; set;}
 
+        public string created_at { get; set; }
+
+        public int user_id { get; set; }
+
+        //missing representation of "user"
+
         public string title { get; set; }
 
-        public string created_at { get; set; }
+        public string permalink_url { get; set; }
+
+        public string uri { get; set; }
+
+        public string artwork_url { get; set; }
+
+        public string description { get; set; }
+
+        //missing representation of "label"
+
+        public int duration { get; set; }
+
+        public string genre { get; set; }
+
+        public string release_day { get; set; }
+
+        public string release_month { get; set; }
+
+        public string release_year { get; set; }
+
+        public bool streamable { get; set; }
+
+        public string stream_url { get; set; }
     }
 }
