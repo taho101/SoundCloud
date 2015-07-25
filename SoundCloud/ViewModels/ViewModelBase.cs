04e43164e2af2bb0c9867624a91ffa83fef97e8e
taho101 <tahirski@gmail.com>
2015-07-25 21:32:43 +0200

    Added base ViewModel

---
+++ SoundCloud/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundCloud.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T property, T value, string propertyName)
+        {
+            if (object.Equals(property, value))
+                return;
+
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
